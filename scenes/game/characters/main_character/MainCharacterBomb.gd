019cc50a01e45b81465a1d16ad5a156c070d3a35
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-21 07:58:23 -0400

    Update MainCharacterBomb.gd

--- scenes/game/characters/main_character/MainCharacterBomb.gd
+++ scenes/game/characters/main_character/MainCharacterBomb.gd
@@ -1,12 +1,12 @@
 extends Node2D
-## Controlador de lanzamiento de bombas.
+## Bomb throwing controller.
 ##
-## Escucha la tecla de tirar bombas y tira la bomba
+## Listens to the bomb throw key and throws the bomb.
 
 
-# Precargamos la escena de la bomba
+# We preload the bomb scene
 var _bomb = preload("res://scenes/game/levels/objects/damage_object/bomb/bomb.tscn")
-# El script de movimiento
+# The movement script
 var _move_script: Node2D
 
 
@@ -17,19 +17,19 @@ func _ready():
 
 # Called when the node enters the scene tree for the first time.
 func _unhandled_input(event):
-	# Validamos cuantas bombas tenemos
+	#  We validate how many bombs we have
 	var _count_bomb = HealthDashboard.points["Bomb"]
-	# Cuando se presiona la tecla (B - bomb) y no tiramos la bomba antes
+	# When the key (B - bomb) is pressed and we did not throw the bomb before
 	if event.is_action_released("bomb") and not _move_script.bombing and _count_bomb > 0:
-		# Quitamos la bomba del invetario
+		# We remove the bomb from the inventory
 		HealthDashboard.add_bomb(-1)
-		# Inicializamos la bomba
+		# We initialize the bomb
 		var bomb_scene = _bomb.instantiate()
-		# Seteamos la posisión a la par del personaje principal
+		# We set the position next to the main character
 		var _character = get_parent()
 		var _character_position = _character.position
 		bomb_scene.position = _character_position
-		# Seteamos la dirección de la fuerza y offset
+		# We set the direction of the force and offset
 		if _move_script.turn_side == "right":
 			bomb_scene.linear_velocity.x = abs(bomb_scene.linear_velocity.x)
 			bomb_scene.position.x = _character_position.x + 25
@@ -37,5 +37,5 @@ func _unhandled_input(event):
 			bomb_scene.linear_velocity.x = - abs(bomb_scene.linear_velocity.x)
 			bomb_scene.position.x = _character_position.x - 25
 	
-		# Agregamos la bomba a la escena
+		# We add the bomb to the scene
 		get_parent().get_parent().add_child(bomb_scene)
