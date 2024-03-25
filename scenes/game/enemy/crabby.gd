4265197c05ff8f1cf46d25b907065bfb71b2f00c
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-25 17:12:26 -0400

    Update crabby.gd

--- scenes/game/enemy/crabby.gd
+++ scenes/game/enemy/crabby.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
-## Clase que controla animación y configuración del Enemigo
+## Class that controls animation and configuration of the Enemy
 ##
-## Setea la animación y comportamiento del Enemigo 
+## Sets the animation and behavior of the Enemy
 
 
-# Acciones del Enemigo
+
+# Actions of the enemy
 @export_enum(
 	"idle",
 	"run",
 ) var animation: String
 
-# Dirección de movimiento del Enemigo
+# Movement direction of the Enemy
 @export_enum(
 	"left",
 	"right",
 	"active",
 ) var moving_direction: String
 
-# Variable para control de animación y colisiones
+# Variable for animation and collision control
 @onready var _animation := $EnemyAnimation
 @onready var _animation_effect := $EnemyEffect
 @onready var _raycast_terrain := $Area2D/RayCastTerrain
@@ -26,54 +27,54 @@ extends CharacterBody2D
 @onready var _raycast_vision_right := $Area2D/RayCastVisionRight
 @onready var _audio_player= $AudioStreamPlayer2D # Reproductor de audios
 
-# Definimos sonidos
+# Define sounds
 var _punch_sound = preload("res://assets/sounds/punch.mp3")
 var _male_hurt_sound = preload("res://assets/sounds/male_hurt.mp3")
 
-# Definición de parametros de física
+# Define physics parameters
 var _gravity = 10
 var _speed = 25
-# Definición de dirección de movimientos
+# Define movement direction
 var _moving_left = true
-# Copia de objeto que entra a colisión
+# Copy of object that enters collision
 var _body: Node2D
-# Vandera de persecución
+# Pursuit flag
 var _is_persecuted = false
-# Vandera de no detectar colisiones
+# Flag for not detecting collisions
 var _stop_detection = false
-# Vandera de no detectar ataques
+# Flag for not detecting attacks
 var _stop_attack = false
-# Cuantas veces aguanta
+# How many hits it can take
 var _hit_to_die = 3
-# Cuantas veces pegaron al personaje principal
+# How many times the main character has hit
 var _has_hits = 0
-# La muerte del cangrejo
+# Crab's death
 var die = false
 
 
-# Función de inicialización
+# Initialization function
 func _ready():
-	# Seteamos la direccion de movimiento
+	# Set the movement direction
 	if moving_direction == 'right':
 		_moving_left = false
 		scale.x = -scale.x
-	# Si no seteamos la animación ponemos por defecto la animación idle
+	# If animation is not set, default to idle animation
 	if not animation:
 		animation = "idle"
-	# Iniciamos la animación
+	# Start the animation
 	_init_state()
 
 
 func _physics_process(delta):
 	if (die): return
-	# Si la animación es de correr, aplicamos el movimiento
+	# If the animation is running, apply movement
 	if animation == "run":
 		_move_character(delta)
 		_turn()
-	# Si la animación es de idle, aplicamos el movimiento
+	# If the animation is idle, apply idle movement
 	elif animation == "idle":
 		_move_idle()
-	# Si la animación es de persecución, aplicamos la persecución
+	# If the animation is pursuit, apply pursuit
 	if moving_direction == "active" and !_stop_detection:
 		_detection()
 
@@ -93,75 +94,75 @@ func _move_character(_delta):
 
 
 func _move_idle():
-	# Aplicamos la gravidad
+	# Apply gravity
 	velocity.y += _gravity
-	# Aplicamos la dirección de movimiento
+	# Apply movement direction
 	velocity.x = 0
-	# Iniciamos el movimiento
+	# Start movement
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
-	# Validamos si la colición es con el personaje principal
+	# Check if the collision is with the main character
 	if body.is_in_group("player"):
 		_stop_detection = true
-		# Atacamos
+		# Attack
 		_attack()
-		# Creamos la copia de objeto
+		# Create object copy
 		_body = body
 
 
 func _on_area_2d_body_exited(__body):
 	if not die:
-		# Estado inicial
+		# Initiate state
 		_init_state()
 
 
 func _turn():
-	# Validamos si termino el terreno
+	# Check if the terrain has ended
 	if not _raycast_terrain.is_colliding() or _raycast_wall.is_colliding():
 		var _object = _raycast_wall.get_collider()
 		if not _object or _object and not _object.is_in_group("player"):
-			# Damos la vuelta
+			# Turn around
 			_moving_left = !_moving_left
 			scale.x = -scale.x
 
 
 func _attack():	
-	# No atacamos si se seteó la banderita _stop_attack
+	# Do not attack if _stop_attack flag is set
 	if _stop_attack:
 		return
 		
 	if not _body:
-		# Esperamos 1 segundos
+		# Wait for 1 second
 		await get_tree().create_timer(0).timeout
 		_attack()
 		
-	# Animación de atacar
+	# Attack animation
 	_animation.play("attack")
 
 
 func _init_state():
 	if _stop_attack:
 		return
-	# Animación de estado inicial
+	# Initial state animation
 	velocity.x = 0
 	_animation.play(animation)
 	_animation_effect.play("idle")
-	# Limpiamos las variables
+	# Clear variables
 	_body = null
 	_stop_detection = false
 
 func _on_enemy_animation_frame_changed():
 	if _stop_attack:
 		return
-	# Validamos si el frame de animación es 0
+	# Check if the animation frame is 0
 	if _animation.frame == 0 and _animation.get_animation() == "attack":
-		# Pegamos al personaje
+		# Hit the character
 		_animation_effect.play("attack_effect")
 		
 		if HealthDashboard.life > 0:
-			# Reproducimos sonido
+			# Play sound
 			_audio_player.stream = _male_hurt_sound
 			_audio_player.play()
 		else:
@@ -169,60 +170,60 @@ func _on_enemy_animation_frame_changed():
 			_animation_effect.play("idle")
 		
 		if _body:
-			# Quitamos vidas
+			# Remove lives
 			var _move_script = _body.get_node("MainCharacterMovement")
 			_move_script.hit(2)
 
 
 func _detection():
-	# Si ya no hay tierra regresamos al estado inicial
+	# If there is no ground, return to initial state
 	if not _raycast_terrain.is_colliding():
-		# Iniciamos la animación
+		# Start animation
 		_init_state()
 		return
-	# Obtenemos los colaiders
+	# Get colliders
 	var _object1 = _raycast_vision_left.get_collider()
 	var _object2 = _raycast_vision_right.get_collider()
 	
-	# Validamos si la colisión es del lado izquerdo
+	# Check if the collision is on the left side
 	if _object1 and _object1.is_in_group("player") and _raycast_vision_left.is_colliding():
 		_move(true)
 	else:
 		_is_persecuted = false
 	
-	# Validamos si la colisión es del lado derecho
+	# Check if the collision is on the right side
 	if _object2 and _object2.is_in_group("player") and _raycast_vision_right.is_colliding():
 		_move(false)
 	
-	# No hay colisiones
+	# No collisions
 	if not _object1 and not _object2 and _animation.get_animation() != "attack":
 		_is_persecuted = false
 		
 		
 func _move(_direction):
-	# Si ya estamos en acción salimos
+	# If we are already in action, exit
 	if _is_persecuted or _animation.get_animation() == "attack":
 		return
-	# Aplicamos la gravidad
+	# Apply gravity
 	velocity.y += _gravity
 	
-	# Volteamos al personaje
+	# Turn the character
 	if not _direction:
 		_moving_left = !_moving_left
 		scale.x = -scale.x
 	else:
-		# Aplicamos la dirección de movimiento
+		# Apply movement direction
 		if _moving_left:
 			velocity.x = - _speed * 5
 		else:
 			velocity.x = _speed * 5
 
-	# Iniciamos el movimiento
+	# Start movement
 	move_and_slide()
 
 
 func _on_area_2d_area_entered(area):
-	# Si estan atacando al enemigo
+	# If the enemy is being attacked
 	if area.is_in_group("hit"):
 		_damage()
 	elif area.is_in_group("die"):
@@ -230,32 +231,32 @@ func _on_area_2d_area_entered(area):
 		_damage()
 
 func _damage():	
-	# Agregamos un golpe
+	# Add a hit
 	_has_hits += 1
-	# Reproducimos sonido
+	# Play sound
 	_audio_player.stream = _punch_sound
 	_audio_player.play()
-	# Reproducimos la animación de pegar
+	# Play hit animation
 	_animation.play("hit")
 	_animation_effect.play("idle")
 	
-	# Validamos si tenemos ataque especial
+	# Check if we have a special attack
 	if Global.number_attack > 0:
-		# Restamos 1 al ataque especial
+		# Subtract 1 from the special attack
 		die = true
 		Global.number_attack -= 1
 	
-	# Validamos si ya no tenemos ataque
+	# Check if we no longer have an attack
 	if Global.number_attack == 0:
-		# Seteamos el ataque normal
+		# Set the normal attack
 		Global.attack_effect = "normal"
 
 	if die or _hit_to_die <= _has_hits:
-		# Seteamoas banderita no atacar
+		# Set flag not to attack
 		_stop_attack = true
 		die = true
 		velocity.x = 0
-		# Lo matamos y quitamos de la escena
+		# Kill it and remove from the scene
 		if _animation.animation != "dead_ground":
 			_animation.play("dead_ground")
 
@@ -267,6 +268,6 @@ func _on_enemy_animation_animation_finished():
 		if not _stop_attack: 
 			_animation.play("idle")
 			_animation_effect.play("idle")
-			# Atacamos
+			# Attack
 			_attack()
 	
