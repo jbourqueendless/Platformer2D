39738a57bf029f659599c781bb9c8dd57915f7a9
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 10:09:00 -0400

    Update scene_1.gd

--- scenes/game/levels/rooms/scene_1/scene_1.gd
+++ scenes/game/levels/rooms/scene_1/scene_1.gd
@@ -1,23 +1,23 @@
 extends Node2D
-## Script de escena principal.
+## Main scene script.
 ##
-## Tiene variables y funciones relacionados con la escena, por ejemplo cambios hacia otras escenas
-## Cambio de escenas: https://docs.google.com/document/d/1eIBtgr8wln1pT0aZ4c-YWk_pqngyBg4HDsgdYLAXv28/edit?usp=sharing
-## Uso de señales: https://docs.google.com/document/d/1vFSOuJkBy7xr5jksgCBNaTpqJHE_K87ZNafB5ZJ_I0M/edit?usp=sharing
+## Contains variables and functions related to the scene, such as changes to other scenes
+## Scene changes: https://docs.google.com/document/d/1eIBtgr8wln1pT0aZ4c-YWk_pqngyBg4HDsgdYLAXv28/edit?usp=sharing
+## Use of signals: https://docs.google.com/document/d/1vFSOuJkBy7xr5jksgCBNaTpqJHE_K87ZNafB5ZJ_I0M/edit?usp=sharing
 
 
-# Area para siguiente nivel
+# Area for the next level
 @onready var _area_next_level = $Areas/AreaNextLevel
 
 
-# Función de inicialización
+# Initialization function
 func _ready():
-	# Escuchamos cuando el personaje entre al área de contacto
+	# Listen for when the character enters the contact area
 	_area_next_level.body_entered.connect(_load_nex_level)
 
 
-# Cargamos el siguiente nivel (la siguiente escena)
+# Load the next level (the next scene)
 func _load_nex_level(body):
 	if body.is_in_group("player"):
 		var scene = "res://scenes/game/levels/rooms/scene_2/scene_2.tscn"
-		#SceneTransition.change_scene(scene) # Por el momento no usaremos este cambio de nivel
+		#SceneTransition.change_scene(scene) # For now, we will not use this scene change
