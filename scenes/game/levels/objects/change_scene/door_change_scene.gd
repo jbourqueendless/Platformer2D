2c8ed04fb474536d480b3fe5142b5512afafe1ee
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 08:14:43 -0400

    Update door_change_scene.gd

--- scenes/game/levels/objects/change_scene/door_change_scene.gd
+++ scenes/game/levels/objects/change_scene/door_change_scene.gd
@@ -1,26 +1,26 @@
 extends Node2D
-## Script de objeto para cambio de escena.
+## Scene change object script.
 ##
-## Es un nodo que representa un objeto y al entrar en contacto cambia a una siguiente escena
-## Cambio de escenas: https://docs.google.com/document/d/1eIBtgr8wln1pT0aZ4c-YWk_pqngyBg4HDsgdYLAXv28/edit?usp=sharing
-## Uso de señales: https://docs.google.com/document/d/1vFSOuJkBy7xr5jksgCBNaTpqJHE_K87ZNafB5ZJ_I0M/edit?usp=sharing
-## Uso de objetos para cambio de escena: https://docs.google.com/document/d/1DeAuU4dYa7DsWs-ht5Aiq4mFraOOu7hraNgIeSZn4lA/edit?usp=sharing
+## It is a node that represents an object and when it comes into contact it changes to a next scene
+## Scene changes: https://docs.google.com/document/d/1eIBtgr8wln1pT0aZ4c-YWk_pqngyBg4HDsgdYLAXv28/edit?usp=sharing
+## Use of signals: https://docs.google.com/document/d/1vFSOuJkBy7xr5jksgCBNaTpqJHE_K87ZNafB5ZJ_I0M/edit?usp=sharing
+## Use of objects for scene change: https://docs.google.com/document/d/1DeAuU4dYa7DsWs-ht5Aiq4mFraOOu7hraNgIeSZn4lA/edit?usp=sharing
 
 
-# Ruta de la escena a cargar
+# Path to the scene to load
 @export var _path_to_scene = ""
 
-# Referencia al area
+# Reference to the area
 @onready var _area = $Area2D
 
 
-# Función de inicialización
+# Initialization function
 func _ready():
 	_area.body_entered.connect(_load_nex_level)
 
 
-# Cargamos el siguiente nivel (la siguiente escena)
+# Load the next level (the next scene)
 func _load_nex_level(body):
-	# Cambiamos de escena si la ruta no está vacía y el personaje principa entra en contacto
+	# Change scene if the path is not empty and the main character comes into contact
 	if _path_to_scene != "" and body.is_in_group("player"):
 		SceneTransition.change_scene(_path_to_scene)
