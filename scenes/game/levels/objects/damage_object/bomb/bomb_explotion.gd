b0f0bcc754ac3897048411ae17295c46ffba6895
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 08:19:38 -0400

    Update bomb_explotion.gd

--- scenes/game/levels/objects/damage_object/bomb/bomb_explotion.gd
+++ scenes/game/levels/objects/damage_object/bomb/bomb_explotion.gd
@@ -1,28 +1,28 @@
 extends AnimatedSprite2D
-## Clase que controla la exploción
+## Class that controls the explosion
 ##
-## Elemina la escena despues de exploción, hace daño
+## Removes the scene after explosion, deals damage
 
 
 @onready var _collision := $Area2D/DamageCollision # Colicionador de la bomba
 
 
 func _on_audio_stream_player_finished():
-	# Eliminamos la escena
+	# Remove the scene
 	queue_free()
 
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
-		# Quitamos vidas
+		# Remove lives
 		var _move_script = body.get_node("MainCharacterMovement")
 		_move_script.hit(10)
 
 
 func _on_frame_changed():
-	# Si el frame es 1 habilitamos el colicionador
+	# If the frame is 1, enable the collision detector
 	if frame == 1:
 		_collision.disabled = false
 	else:
-		# Deshabilitamos el colicionador
+		# Disable the collision detector
 		_collision.disabled = true
