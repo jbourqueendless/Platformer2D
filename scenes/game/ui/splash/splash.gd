cc20983012415c26131f44606d35590b49df6a26
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 11:00:04 -0400

    Update splash.gd

--- scenes/game/ui/splash/splash.gd
+++ scenes/game/ui/splash/splash.gd
@@ -1,49 +1,49 @@
 extends Node2D
-## Clase que controla la escena principal (Splash) 
+## Class that controls the main scene (Splash)
 ## 
-## Cambia la escena actual por mapa global del juego, hace animación de logos, dueños del juego 
+## Changes the current scene to the global map of the game, animates logos, game owners 
 
 
 @onready var AnimationPlayerGP360: AnimationPlayer = $AnimationPlayerGP360
 @onready var AnimationPlayerEndless: AnimationPlayer = $AnimationPlayerEndless
 @onready var AnimationPixelFrog: AnimationPlayer = $AnimationPixelFrog
 
-# Ruta a la escena a cargar cuando finalice el "splash"
+# Path to the scene to load when the "splash" is finished
 var _path_map_scene = "res://scenes/game/levels/rooms/init/init.tscn"
 
 
 func _ready():
-	# Escondemos la escena del menu
+	# Hide the menu scene
 	HealthDashboard.visible = false
-	# Mostramos el logo de Endless
+	# Show the Endless logo
 	AnimationPlayerEndless.play("do_splash")
 
 
-# Escuchamos el teclado
+# Listen for keyboard input
 func _input(event):
-	# Escuchamos si se preciona algun boton
+	# Listen if any button is pressed
 	if event is InputEventKey:
-		# Llamamos el la función de cambio de escena
+		# Call the scene change function
 		_go_title_screen()
 
 
-# Cuando termina la animación
+# When the animation is finished
 func _on_animation_player_animation_finished(_anim_name):
 	# Mostramos el logo de Pixel Frog
 	AnimationPixelFrog.play("do_splash")
 
 
-# Redirect a la escena de Mapa
+# Redirect to the Map scene
 func _go_title_screen():
-	# Pasamos a la escena de Menú principal
+	# Move to the main menu scene
 	SceneTransition.change_scene(_path_map_scene)
 
 
 func _on_animation_player_endless_animation_finished(anim_name):
-	# Mostramos el logo de GP360
+	# Show the GP360 logo
 	AnimationPlayerGP360.play("do_splash")
 
 
 func _on_animation_pixel_frog_animation_finished(anim_name):
-	# Llamamos el la funcion de cambio de escena
+	# Call the scene change function
 	_go_title_screen()
