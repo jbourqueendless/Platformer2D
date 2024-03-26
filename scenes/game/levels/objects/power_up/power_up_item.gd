b7bdf7e582843cb3d1890b84159e05c8f3ebb2b4
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 09:23:21 -0400

    Update power_up_item.gd

--- scenes/game/levels/objects/power_up/power_up_item.gd
+++ scenes/game/levels/objects/power_up/power_up_item.gd
@@ -1,39 +1,39 @@
 extends Node2D
-## Script de objetos de inventario
+## Inventory item script
 ##
-## Este objeto al utilizarse, mostrará un diálogo informativo
-## Creación de items: https://docs.google.com/document/d/1XY3Y5Q1njEV8fwL3x4ogLF6Nyz01gobjJ7mr_Vh-jPE/edit?usp=sharing
-## Utilización de items: https://docs.google.com/document/d/1zjo0Deoc_yy-wdmGRd9rvUbNIDHhNxRlmNxb8qhc5Xg/edit?usp=sharing
+## This object, when used, will show an informative dialogue
+## Item creation: https://docs.google.com/document/d/1XY3Y5Q1njEV8fwL3x4ogLF6Nyz01gobjJ7mr_Vh-jPE/edit?usp=sharing
+## Item usage: https://docs.google.com/document/d/1zjo0Deoc_yy-wdmGRd9rvUbNIDHhNxRlmNxb8qhc5Xg/edit?usp=sharing
 
 
 var _is_active = false # Para saber si el puntero está en el item
-# Mapeo de animación y diálogos
+# Animation and dialogues mapping
 var _dialogues = {
 	"blue_potion": "res://scenes/game/dialogues/dialogues/power_up/blue_potion_item.dialogue",
 	"green_bottle": "res://scenes/game/dialogues/dialogues/power_up/green_bottle_item.dialogue",
 }
 
-@export var num = "1" # Numero de conteo
-@onready var canvas = $Num # Contador
-@onready var animation = $PowerUpItem # Animación
-@onready var _confirm = $CanvasConfirm # Confirmación
+@export var num = "1" # Count number
+@onready var canvas = $Num # Counter
+@onready var animation = $PowerUpItem # Animation
+@onready var _confirm = $CanvasConfirm # Confirmation dialogue
 
 
-# Función de inicialización
+# Initialization function
 func _ready():
 	canvas.text = num
 
 
-# Función para detectar eventos de teclado y ratón
+# Function to detect keyboard and mouse events
 func _unhandled_input(event):
 	if not _is_active:
-		return # Si el puntero no está sobre el item, terminamos la función
-	# Si hacemos clic, mostramos el diálogo de confirmación
+		return # If the pointer is not over the item, end the function
+	# If we click, show the confirmation dialogue
 	if event is InputEventMouseButton and event.is_action_released("clic"):
 		_confirm.show()
 
 
-# Mostramos el diálogo
+# Show the dialogue
 func _show_dialogue():
 	var _resource = _dialogues[animation.animation]
 	var _instance = load(_resource)
@@ -41,39 +41,39 @@ func _show_dialogue():
 	_dialogue.on_dialogue_ended(_on_dialogue_ended)
 
 
-# Escuchamos cuando termina el diálogo
+# Listen for when the dialogue ends
 func _on_dialogue_ended():
-	# Restamos el item del inventario
+	# Remove the item from the inventory
 	InventoryCanvas.remove_item_by_name(animation.animation)
 
-# Actualizamos la cantidad disponible
+# Update the available quantity
 func set_num(_num: String):
 	canvas.text = _num
 
 
-# Obtenemos la cantidad disponible
+# Get the available quantity
 func get_num():
 	return int(canvas.text)
 
 
-# Escuchamos cuando el puntero entra en el item
+# Listen for when the pointer enters the item
 func _on_area_mouse_entered():
 	_is_active = true
 
 
-# Escuchamos cuando el puntero sale del item
+# Listen for when the pointer leaves the item
 func _on_area_mouse_exited():
 	_is_active = false
 
 
-# Cuando se presiona el botón "cancelar"
+# When the "cancel" button is pressed
 func _on_cancel_pressed():
 	_confirm.hide()
 
 
-# Cuando se presiona el botón "aceptar"
+# When the "accept" button is pressed
 func _on_accept_pressed():
-	# Activamos el poder
+	# Activate the power
 	Global.attack_effect = animation.animation
 	Global.number_attack = 4
 	_confirm.hide()
@@ -81,6 +81,6 @@ func _on_accept_pressed():
 	_show_dialogue()
 
 
-# Obtenemos la nombre del objeto
+# Get the object name
 func set_animation(animation_name):
 	animation.play(animation_name)
