aac2ba80701f307d93888ca7aa29857f8c1e149f
jbourqueendless <164198892+jbourqueendless@users.noreply.github.com>
2024-03-26 08:41:13 -0400

    Update pickable_object.gd

--- scenes/game/levels/objects/pickable_object/pickable_object.gd
+++ scenes/game/levels/objects/pickable_object/pickable_object.gd
@@ -1,11 +1,11 @@
 extends Area2D
-## Clase que controla animación y configuración de los objetos recolectables
+## Class that controls animation and configuration of collectible objects
 ##
-## Setea la animación del objeto segun el nombre configurado
-## Cambia animación de idle a recolectado, elimina objeto recolectado de la escena
+## Sets the animation of the object according to the configured name
+## Changes animation from idle to collected, removes collected object from the scene
 
 
-# Nombre del personaje principal
+# Name of the main character
 @export_enum(
 	"blue_diamond",
 	"green_diamond", 
@@ -14,53 +14,53 @@ extends Area2D
 	"silver_coin",
 ) var animation: String
 
-# Definimos el sprite animado de la moneda
+# Define the animated sprite of the coin
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var _audio_player= $AudioStreamPlayer2D # Reproductor de audios
 
 var _pickup_sound = preload("res://assets/sounds/pickup.mp3")
 
 
-# Función de carga del nodo
+# Function to load the node
 func _ready():
 	if not animation:
 		return
 	
-	# Cargamos las texturas de animación según el nombre configurado
+	# Load animation textures according to the configured name
 	var _animation1 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/01.png"
 	var _animation2 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/02.png"
 	var _animation3 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/03.png"
 	var _animation4 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/04.png"
 
-	# Aplicamos la textura cargada a la animación
+	# Apply the loaded texture to the animation
 	_animated_sprite.sprite_frames.set_frame("idle", 0, load(_animation1))
 	_animated_sprite.sprite_frames.set_frame("idle", 1, load(_animation2))
 	_animated_sprite.sprite_frames.set_frame("idle", 2, load(_animation3))
 	_animated_sprite.sprite_frames.set_frame("idle", 3, load(_animation4))
 	
-	# Reproducimos la animación idle
+	# Play idle animation
 	_animated_sprite.play("idle")
 	
 
 func _on_animated_sprite_2d_animation_finished():
-	# Esperamos 2 segundos 
+	# Wait for 2 seconds  
 	await get_tree().create_timer(2).timeout
-	# Eliminamos el objeto recogido de la escena
+	# Remove the collected object from the scene
 	queue_free()
 	
 	
 func do_animation():
-	# Validamos si la animación es de moneda
+	# Check if the animation is for a coin
 	_audio_player.stream = _pickup_sound
 	_audio_player.play()
 	if animation == "gold_coin" or animation == "silver_coin":
-		# Reproducimos la animación de la moneda
+		# Play the coin animation
 		_animated_sprite.play("coin_taken")
 	else:
-		# Reproducimos la animación del diamante
+		# Play the diamond animation
 		_animated_sprite.play("diamond_taken")
 		
-	# Sumar los objetos recolectados
+	# Add the collected objects
 	var type = "GoldCoin"
 	if animation == "silver_coin":
 		type = "SilverCoin"
@@ -70,19 +70,19 @@ func do_animation():
 		type = "GreenDiamond"
 	elif animation == "red_diamond":
 		type = "RedDiamond"
-	# Todos los diferente tipos de objetos suman 1 unidad
+	# All different types of objects add 1 unit
 	HealthDashboard.add_points(type, 1) 
 
 
 func _on_area_entered(area):
-	# Validamos que la colisión es con el personaje principal 
+	# Check if the collision is with the main character 
 	if area.is_in_group("player"):
-		# Reproducimos la animación de la moneda recogida
+		# Play the animation of the collected coin
 		do_animation()
 
 
 func _on_body_entered(body):
-	# Validamos que la colisión es con el personaje principal 
+	# Check if the collision is with the main character
 	if body.is_in_group("player"):
-		# Reproducimos la animación de la moneda recogida
+		# Play the animation of the collected coin
 		do_animation()
